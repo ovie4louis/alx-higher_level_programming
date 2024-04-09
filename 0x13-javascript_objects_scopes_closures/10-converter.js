e7b69102445af43e4e9767034e7c37228cc9c95d
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2024-04-09 08:20:36 -0700

    object

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
