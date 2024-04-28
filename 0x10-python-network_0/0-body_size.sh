900bb3249d7ceca92a4e2325802aa23e1d669792
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2024-04-28 02:55:17 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-#!/bin/bash
-
-# Check if URL is provided as argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to the URL using curl and store the response body
-response_body=$(curl -sI "$1" | grep -i content-length | awk '{print $2}')
+# Sends a GET request to the URL, and displays the body of the response
+curl -s -L "${1}"
