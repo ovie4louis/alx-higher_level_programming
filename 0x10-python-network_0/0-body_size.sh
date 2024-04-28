63a21ac09b0705ece57777b847beeb58ea125b58
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2024-04-28 02:30:23 -0700

    task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-#  Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
 
-# Get the URL from the command line argument
-url=$1
-
-# Send a request to the URL and get the size of the body
-size=$(curl -sI "$url" | grep -i 'Content-Length' | awk '{print $2}')
-
-# Display the size of the body in bytes
-echo "Size of the body: $size bytes"
-
+GET /index.html HTTP/1.1
+host: www.ovie4lead.tech
+content-length: 50
